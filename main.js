626e6a284b159cc4bf376f694da232b79ca216a4
Heather Kraft <heather.kraft@colorado.edu>
2020-09-20 11:13:45 -0600

    finalized spacing and layout. Working on the functionality of the button

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 // holder place for javascript
 
 
+// function JokeAnswer() {
+    // document.getElementById("Answer").style.display = "";
+// }
+
+let jokeanswerList = document.getElementsByTagName('answer');
+let btn = jokeanswerList[0];
+
+function JokeAnswer() {
+      var x = document.getElementById("answer");
+    
+        if (x.style.display 
+    === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = 
+      "none";
+      }
+    } 
